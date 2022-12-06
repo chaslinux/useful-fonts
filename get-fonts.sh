f2ba0228d02f8c6f9bf5a2e3ead1595e63940f57
chaslinux <chaslinux@gmail.com>
2022-12-06 14:10:59 -0500

    Initial commit, a lot of fonts get installed. Needs to be trimmed.

---
+++ get-fonts.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+# getfonts.sh
+# Download and install useful fonts for Xubuntu 20.04
+# Copyright 2022, chaslinux@gmail.com
+# This script is Licensed under GPL v3
+
+# For licensing for each font refer to the /usr/share/doc folder
+# for each font. For example: /usr/share/doc/fonts-adf-baskervald/copyright
+
+# Arkandis Digital Foundry fonts
+sudo apt install -y fonts-3270 fonts-adf-accanthis fonts-adf-baskervald
+sudo apt install -y fonts-adf-berenis fonts-adf-gillius fonts-adf-ikarius
+sudo apt install -y fonts-adf-irianis fonts-adf-libris fonts-adf-mekanus
+sudo apt install -y fonts-adf-oldania fonts-adf-romande fonts-adf-solothurn
+sudo apt install -y fonts-adf-switzera fonts-adf-tribun fonts-adf-universalis
+sudo apt install -y fonts-adf-verana
+
+# Brian Kent font collection (465 Truetype fonts)
+sudo apt install -y fonts-aenigma
+
+# monospaces programming font
+sudo apt install -y fonts-agave
+
+# Alegreya fonts
+sudo apt install -y fonts-alegreya-sans
+
+# signage font
+sudo apt install -y fonts-allerta
+
+# Amiga workbench font
+sudo apt install -y fonts-amiga
+
+# Warsaw type font, decorative for signs
+sudo apt install -y fonts-apropal fonts-bajaderka
+
+# Contemporary modern typeface
+sudo apt install -y fonts-arapey
+
+# replacements for Arial, Times, and Verdana
+sudo apt install -y fonts-arkpandora
+
+# looks like a sloppy stencil painted font
+sudo apt install -y fonts-averia-gwf 
+
+# font for cockpit screens
+sudo apt install -y fonts-b612
+
+# Nice square font 
+sudo apt install -y fonts-babelstone-modern fonts-babelstone-han
+
+# Google font by Ryoichi Tsunekawa
+sudo apt install -y fonts-bebas-neue
+
+# Futura-link sans-serif font
+sudo apt install -y fonts-beteckna
+
+sudo apt install -y fonts-blankenburg
+sudo apt install -y fonts-bpg-georgian
+sudo apt install -y fonts-breip
+sudo apt install -y fonts-bwht
+sudo apt install -y fonts-cabin
+sudo apt install -y fonts-cabinsketch
+sudo apt install -y fonts-cantarell
+sudo apt install -y fonts-cardo
+sudo apt install -y fonts-cegui
+sudo apt install -y fonts-century-catalogue
+sudo apt install -y fonts-cherrybomb
+sudo apt install -y fonts-clear-sans
+sudo apt install -y fonts-comfortaa
+sudo apt install -y fonts-compagnon
+sudo apt install -y fonts-croscore
+sudo apt install -y fonts-crosextra-caladea
+sudo apt install -y fonts-dkg-handwriting
+sudo apt install -y fonts-dosis
+
+# from dustismo.com
+sudo apt install -y fonts-dustin
+
+sudo apt install -y fonts-ebgaramond
+sudo apt install -y fonts-ebgaramond-extra
+sudo apt install -y fonts-elstob
+
+# seems to be emojis
+sudo apt install -y fonts-emojione
+
+sudo apt install -y fonts-engadget
+
+# Dingbats
+sudo apt install -y fonts-entypo
+
+# Wipeout font
+sudo apt install -y fonts-f500
+
+sudo apt install -y fonts-fantasma
+sudo apt install -y fonts-fanwood
+sudo apt install -y fonts-ferrite-core
+
+sudo apt install -y fonts-gamaliel
+sudo apt install -y fonts-georgewilliams
+sudo apt install -y fonts-gnutypewriter
+sudo apt install -y fonts-goudybookletter
+sudo apt install -y fonts-havana
+sudo apt install -y fonts-ibm-plex
+
+
+
+
+
+
+
+
